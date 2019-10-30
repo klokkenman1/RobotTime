7ac13849a78a13a5c454380f49c63645a39724b4
Kelvin Snepvangers <kelvinsnepvangers@gmail.com>
2019-10-30 15:48:44 +0100

    Added Room db

--- app/src/main/java/com/example/robottime/MainActivity.kt
+++ app/src/main/java/com/example/robottime/MainActivity.kt
@@ -1,20 +1,18 @@
 package com.example.robottime
 
 import android.os.Bundle
+import android.util.Log
+import androidx.room.Room
 import com.aldebaran.qi.sdk.QiContext
 import com.aldebaran.qi.sdk.QiSDK
 import com.aldebaran.qi.sdk.RobotLifecycleCallbacks
-import com.aldebaran.qi.sdk.design.activity.RobotActivity
-import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.activity_main.*
 import com.aldebaran.qi.sdk.builder.SayBuilder
-import com.aldebaran.qi.sdk.`object`.conversation.Say
-import androidx.core.app.ComponentActivity
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-
-
+import com.aldebaran.qi.sdk.design.activity.RobotActivity
+import com.example.robottime.data.AppDatabase
+import com.example.robottime.data.User
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 
 class MainActivity:RobotActivity(), RobotLifecycleCallbacks {
@@ -22,13 +20,17 @@ class MainActivity:RobotActivity(), RobotLifecycleCallbacks {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         QiSDK.register(this,this)
+        GlobalScope.launch {
+            val db = Room.databaseBuilder(
+                applicationContext,
+                AppDatabase::class.java, "database-name"
+            ).build()
+
+            //db.userDao().insertAll(User(0,"Kelvin", "Snepvangers"))
+            Log.i("Test", db.userDao().findByName("Kelvin", "Snepvangers").firstName)
+            textView.text = db.userDao().getAll()[0].firstName
+        }
     }
 
     override fun onDestroy() {
